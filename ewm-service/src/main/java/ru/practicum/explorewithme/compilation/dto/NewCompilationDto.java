0f9a44d360adcb824f1c286d2974a350e3014cc4
Timur <bear.timur@yandex.ru>
2024-07-08 18:00:57 +0500

    Замечания устранены, спасибо.

--- ewm-service/src/main/java/ru/practicum/explorewithme/compilation/dto/NewCompilationDto.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/compilation/dto/NewCompilationDto.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.List;
 
@@ -17,6 +18,7 @@ public class NewCompilationDto {
 
     List<Long> events;
     Boolean pinned;
+    @NotBlank
     @Size(min = 1, max = 50, message = "Длина должна быть от 1 до 50 символов")
     String title;
 }
